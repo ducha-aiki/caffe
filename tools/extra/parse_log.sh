b3c060f3bdcc1b4711aab987dcfc428753c962c4
Dmytro Mishkin <ducha.aiki@gmail.com>
2015-09-25 10:02:34 +0300

    Fixed parse_log.sh and better colors in plot_training_log.py.example

--- tools/extra/parse_log.sh
+++ tools/extra/parse_log.sh
@@ -14,7 +14,12 @@ echo "Usage parse_log.sh /path/to/your.log"
 exit
 fi
 LOG=`basename $1`
-grep -B 1 'Test ' $1 > aux.txt
+sed -n '/Iteration .* Testing net/,/Iteration *. loss/p' $1 > aux.txt
+sed -i '/Waiting for data/d' aux.txt
+sed -i '/prefetch queue empty/d' aux.txt
+sed -i '/Iteration .* loss/d' aux.txt
+sed -i '/Iteration .* lr/d' aux.txt
+sed -i '/Train net/d' aux.txt
 grep 'Iteration ' aux.txt | sed  's/.*Iteration \([[:digit:]]*\).*/\1/g' > aux0.txt
 grep 'Test net output #0' aux.txt | awk '{print $11}' > aux1.txt
 grep 'Test net output #1' aux.txt | awk '{print $11}' > aux2.txt
@@ -31,16 +36,16 @@ paste aux0.txt aux4.txt aux1.txt aux2.txt | column -t >> $LOG.test
 rm aux.txt aux0.txt aux1.txt aux2.txt aux3.txt aux4.txt
 
 # For extraction of time since this line contains the start time
-grep '] Solving ' $1 > aux.txt
-grep ', loss = ' $1 >> aux.txt
-grep 'Iteration ' aux.txt | sed  's/.*Iteration \([[:digit:]]*\).*/\1/g' > aux0.txt
-grep ', loss = ' $1 | awk '{print $9}' > aux1.txt
-grep ', lr = ' $1 | awk '{print $9}' > aux2.txt
+grep '] Solving ' $1 > sec_aux.txt
+grep ', loss = ' $1 >> sec_aux.txt
+grep 'Iteration ' sec_aux.txt | sed  's/.*Iteration \([[:digit:]]*\).*/\1/g' > sec_aux0.txt
+grep ', loss = ' $1 | awk '{print $9}' > sec_aux1.txt
+grep ', lr = ' $1 | awk '{print $9}' > sec_aux2.txt
 
 # Extracting elapsed seconds
-$DIR/extract_seconds.py aux.txt aux3.txt
+$DIR/extract_seconds.py sec_aux.txt sec_aux3.txt
 
 # Generating
 echo '#Iters Seconds TrainingLoss LearningRate'> $LOG.train
-paste aux0.txt aux3.txt aux1.txt aux2.txt | column -t >> $LOG.train
-rm aux.txt aux0.txt aux1.txt aux2.txt  aux3.txt
+paste sec_aux0.txt sec_aux3.txt sec_aux1.txt sec_aux2.txt | column -t >> $LOG.train
+rm sec_aux.txt sec_aux0.txt sec_aux1.txt sec_aux2.txt  sec_aux3.txt
